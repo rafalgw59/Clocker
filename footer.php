4f80080eb61a3c5dc98a88d0844ee49c803cbb0e
rafalgw59 <r.gaw59lik@gmail.com>
2022-01-19 21:10:49 +0100

    nothing to see here

--- footer.php
+++ footer.php
@@ -2,5 +2,5 @@
     <p id ="p-footer">Powered by DeltaSzwadronSuperCoolKomandoWilkówAlfa</p>
 </div>
 <script src="scripts/topsecret.js"></script>
-<img id="topsecret" src="assets/topsecret.gif" style="display: none; position:absolute; top:25rem; left:0;" width="400" height="300">
-<img id="topsecret2" src="assets/topsecret2.gif" style="display: none;position:absolute; top:19rem; right:0;" width="528" height="404">
+<img id="topsecret" src="assets/topsecret.gif" style="display: none; position:absolute; bottom:0; left:0;" width="400" height="300">
+<img id="topsecret2" src="assets/topsecret3.gif" style="display: none;position:absolute; bottom:0; right:0;" width="490" height="476">
